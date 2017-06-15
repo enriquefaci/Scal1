aa544d19c0a97d7616458345af8ffab22d4dce45
mert inan <inanme@gmail.com>
2017-06-15 15:23:27 +0100

    updated better Future

--- src/main/scala/org/inanme/MyFuture.scala
+++ src/main/scala/org/inanme/MyFuture.scala
@@ -3,62 +3,45 @@ package org.inanme
 import java.util.concurrent.TimeUnit
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Random, Success}
 
 object MyFuture extends App {
-
-  trait Currency
-
-  case object USD extends Currency
-
-  case object CHF extends Currency
-
-  def isProfitable(curr1: Int, curr2: Int): Boolean = Random.nextBoolean()
-
-  object Connection {
-
-    def getCurrentValue(currency: Currency): Int = {
-      val x = Random.nextInt(5).toLong
-      TimeUnit.SECONDS.sleep(x)
-      if (x == 3l) {
-        throw new IllegalArgumentException("failed")
-      }
-      currency match {
-        case USD => 1
-        case CHF => 2
-        case _ => -1
-      }
+  def blockMe(op: String): Int = {
+    val x = Random.nextInt(5)
+    TimeUnit.SECONDS.sleep(x)
+    if (x == 3l) {
+      throw new IllegalArgumentException("failed " + op)
     }
-
-    def buy(amount: Int, currency: Currency) = ()
+    println(op + " finished")
+    x
   }
 
-  def simulate() = {
-    val usdQuote: Future[Int] = Future {
-      Connection.getCurrentValue(USD)
-    }
-
-    usdQuote.onComplete {
-      case Success(x) => println(x)
-      case Failure(_) => println("Failed to fetch USD")
-    }
-
-    val chfQuote = Future {
-      Connection.getCurrentValue(CHF)
-    }
-
-    chfQuote.onComplete {
-      case Success(x) => println(x)
-      case Failure(_) => println("Failed to fetch CHF")
-    }
-
-    for {
-      usd <- usdQuote
-      chf <- chfQuote
-    } {
-      println(isProfitable(usd, chf))
-    }
+  val op1: Future[Int] = Future {
+    blockMe("op1")
   }
+
+  val op2: Future[Int] = Future {
+    blockMe("op2")
+  }
+
+  op1.onComplete {
+    case Success(x) => println(x)
+    case Failure(_) => println("Failed to fetch op1")
+  }
+
+  op2.onComplete {
+    case Success(x) => println(x)
+    case Failure(_) => println("Failed to fetch op2")
+  }
+
+  val result: Future[Int] = for {
+    v1 <- op1
+    v2 <- op2
+  } yield v1 + v2
+
+  Await.ready(result, Duration.Inf)
+  result map println
 
 }
