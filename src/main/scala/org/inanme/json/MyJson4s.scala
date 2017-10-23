7bc57e235baf446ceaf2cfa9a390cdac1c7eb291
mert inan <inanme@gmail.com>
2017-10-23 22:18:03 +0100

    adding optionals

--- src/main/scala/org/inanme/json/MyJson4s.scala
+++ src/main/scala/org/inanme/json/MyJson4s.scala
@@ -1,28 +1,27 @@
 package org.inanme.json
 
-import org.json4s.JInt
-import org.json4s.JsonAST.{JArray, JField}
+import org.json4s.JsonAST._
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 
 import scala.language.implicitConversions
 
-object MyJson4s
-
 object Part1 extends App {
-
   val r = render(("name", "mert") ~ ("numbers", Range(1, 10).toList)) \ "numbers" match {
     case JArray(x) ⇒ x.collect { case x1: JInt ⇒ x1.num + 10 }
     case _ ⇒ None
   }
-
   println(r)
-
   println(compact(List(1, 2, 3)))
   println(pretty(Seq(1, 2, 4)))
 
+  def some1: Option[JValue] = Some("get" → "it")
+  def none1: Option[JValue] = None
+
   val name: JField = "name" -> "mert"
   val surname: JField = "surname" -> "inan"
-  println(compact(name ~ surname))
+  val option1: JField = "some1" -> some1
+  val option2: JField = "none1" -> none1
+  println(compact(name ~ surname ~ option1 ~ option2))
 
 }
